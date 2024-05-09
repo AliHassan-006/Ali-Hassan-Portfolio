6e37ddd2943694495e54ed18fa91a52930d4a210
Ali Hassan <168643012+AliHassan-006@users.noreply.github.com>
2024-05-09 18:28:56 +0500

    Resume

---
+++ files/script.js
@@ -0,0 +1,25 @@
+
+var typed = new Typed(".text", {
+    strings: ["Web Development" , "Technical Work" , "Graphic Designing"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+const switchElement = document.querySelector('.switch')
+
+switchElement.addEventListener('click', () => {
+ document.body.classList.toggle('dark')
+ })
